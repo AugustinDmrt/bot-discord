7408255e4d4893de891f0413717a4bc59e7107aa
TotoLec <55685649+TotoLec@users.noreply.github.com>
2022-01-29 18:24:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const Discord = require('discord.js');
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+const Discord = require("discord.js");
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 require("dotenv").config();
 
-const prefixCmd = 'd!';
+const prefixCmd = "d!";
 
 //Toutes les actions à faire quand le bot se connecte
 client.on("ready", function() {
     console.log("Bot ON");
-})
+});
 
-client.on("messageCreate", msg => {
-
+client.on("messageCreate", (msg) => {
     // Si le message n'est pas préfixé ou qu'il vient d'un autre bot, nous l'ignorons
-    if (!msg.content.startsWith(prefixCmd) || msg.author.bot) return
+    if (!msg.content.startsWith(prefixCmd) || msg.author.bot) return;
 
     // Si nous arrivons jusque ici, alors c'est une commande
 
@@ -37,12 +36,13 @@ client.on("messageCreate", msg => {
         default:
             break;
     }
-
 });
 
 // Si un personne leave le server :
-guild.leave()
-    .then(client.channels.cache.get("937026983265726495").send("Left the guild ${g}"))
-
+client
+    .leave()
+    .then(
+        client.channels.cache.get("937026983265726495").send("Left the guild ${g}")
+    );
 
 client.login(process.env.BOT_TOKEN);
