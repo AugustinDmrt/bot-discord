06db8040880880f791985726460e9b09a1aade92
TotoLec <55685649+TotoLec@users.noreply.github.com>
2022-01-29 22:08:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const client = new Discord.Client({
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MEMBERS,
+        /*Intents.FLAGS.GUILD_MEMBERS,*/
     ],
 });
 require("dotenv").config();
