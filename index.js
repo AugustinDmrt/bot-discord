a7fe7f5acafed7ad2d8bf0fb29d26798c5d9f289
TotoLec <55685649+TotoLec@users.noreply.github.com>
2022-01-31 21:06:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ client.on("ready", function() {
 
 client.on("messageCreate", (msg) => {
     // Anti raciste section ---------------------------------------------------------------------------
-    var heSaid = msg.content.shift().toLowerCase();
+    var heSaid = msg.content.toLowerCase();
     for (var i = 0; i < x.length; i++) {
         if (banWorld[i] === heSaid) {
             msg.member.roles.add("935240847639851019");
