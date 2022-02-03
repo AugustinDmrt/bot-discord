9918be0a381dec8967c163e3267192423ca5dd7b
TotoLec <55685649+TotoLec@users.noreply.github.com>
2022-02-03 13:10:44 +0100

    test

--- index.js
+++ index.js
@@ -27,9 +27,7 @@ client.on("ready", function() {
 
     let rawdata = fs.readFileSync(path.resolve(__dirname, 'database.json'));
     let data = JSON.parse(rawdata);
-    console.log(data[0][326660584848490496].username);
-    console.log(data[0][326660584848490496].xp);
-    console.log(data[0][326660584848490496].level);
+    console.log(data[0][12345].level);
     console.log("-----------");
 });
 
