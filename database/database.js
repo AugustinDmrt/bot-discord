11e31bfd1d7e89ff4e66dd16e27bd7b0161a80e4
TotoLec <55685649+TotoLec@users.noreply.github.com>
2022-02-15 13:31:39 +0100

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,5 +1,4 @@
 const { Sequelize } = require('sequelize')
-const  { Sequelize } = require('../config/db.js'); //sequelize instance
 
 module.exports = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: '217.160.186.162',
