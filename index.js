bd18a45d269b50a06da09c95481115d55aed018b
TotoLec <55685649+TotoLec@users.noreply.github.com>
2022-02-15 13:30:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ client.on("ready", function() {
     //     .catch(err => console.log(err));
 
     try {
-        await db.authenticate();
+        db.authenticate();
         console.log('Connection has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
