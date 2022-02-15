83229a2b8c90240fc0bdb201e8b3320d1b07971c
TotoLec <55685649+TotoLec@users.noreply.github.com>
2022-02-15 13:20:15 +0100

    Update database.js

--- database/database.js
+++ database/database.js
@@ -2,6 +2,5 @@ const { Sequelize } = require('sequelize')
 
 module.exports = new Sequelize( process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS, {
     host: '217.160.186.162',
-    database: 'drillzilla',
     dialect: 'mariadb'
 });
